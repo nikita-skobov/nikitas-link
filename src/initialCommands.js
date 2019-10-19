7c1a25cbf0d72922298e832bca42e257090c783a
nikita-skobov <nskobov.websites@gmail.com>
2019-10-19 16:12:14 -0500

    prevents displaying block ascii letters if on mobile

--- src/initialCommands.js
+++ src/initialCommands.js
@@ -15,9 +15,22 @@ const WELCOME = `
     \\/  \\/     |______| |______|  \\_____|  \\____/  |_|  |_| |______|
 `
 
+let htmlFriendlyWelcome = replaceNewlinesWithBreaks(WELCOME.replace(/ /g, '\u00a0'), 'm-0')
+// replaces empty spaces with unicode empty space. no margins in the
+// paragraph children that replaceNewlinesWithBreaks inserts.
+
+const pixelsPerChar = 10
+const welcomeWidth = WELCOME.split('\n')[1].length * pixelsPerChar
+
+if (window.innerWidth < welcomeWidth) {
+    htmlFriendlyWelcome = 'WELCOME!'
+}
+
 const initialTerminalHistory = [
-    <TerminalInput command="cat myfile.txt" />,
-    <div>{replaceNewlinesWithBreaks(WELCOME.replace(/ /g, '\u00a0'), 'm-0')}</div>,
+    <TerminalInput command="./welcome.sh" />,
+    <div>
+        {htmlFriendlyWelcome}
+    </div>,
     <TerminalInput command="dsadsadsadsa" />,
 ]
 
