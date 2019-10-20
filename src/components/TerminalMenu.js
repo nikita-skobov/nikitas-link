01236663797b71fa7c39a1e94463f17d3c7f684e
nikita-skobov <nskobov.websites@gmail.com>
2019-10-19 19:08:28 -0500

    fixes menu left/right alignment

--- src/components/TerminalMenu.js
+++ src/components/TerminalMenu.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export function TerminalMenu(props) {
-    return (
-        <div className="container pr-0 pl-0">
+    return [
+        <div className="container mr-0 pr-0 pl-0">
             <div className="row terminal-window-menu">
                 <div className="menu-padding col pr-0 pl-0" />
                 <div className="menu-padding col pr-0 pl-0 text-center text-white">
@@ -14,6 +14,8 @@ export function TerminalMenu(props) {
                     <span className="pl-1 pr-1 oi oi-x" title="x" aria-hidden="true" /> 
                 </div>
             </div>
+        </div>,
+        <div className="container ml-0 pr-0 pl-0">
             <div className="row terminal-window-menu text-white">
                 <div className="menu-button col-fluid pl-2 pr-3 pb-1 pt-1">File</div>
                 <div className="menu-button col-fluid pl-2 pr-3 pb-1 pt-1">Edit</div>
@@ -22,7 +24,7 @@ export function TerminalMenu(props) {
                 <div className="menu-button col-fluid pl-2 pr-3 pb-1 pt-1">Help</div>
             </div>
         </div>
-    )
+    ]
 }
 
 export default TerminalMenu
