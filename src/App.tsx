6a2e6221bdbc5f5296e75ad91905d4d01c0f7e30
nikita-skobov <nskobov.websites@gmail.com>
2021-05-02 18:58:04 -0500

    starts restyling/layout work

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Helloworld</p>
-        <p>
-          <a
-            className="App-link"
-            href="https://nikitas.link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+      <header>
+        <a href="#">Nikita's Link</a>
+        <nav>
+          <a href="https://github.com/nikita-skobov">Code</a>
+          <a href="https://blog.nikitas.link">Blog</a>
+          <a href="mailto:skobo002@umn.edu">Email</a>
+        </nav>
       </header>
+      <div>
+        <div className="row">
+          <div className="column">
+            <img
+              id="profilepic"
+              src="https://avatars.githubusercontent.com/u/39128800?v=4"
+              alt="aaaa"
+            />
+          </div>
+          <div className="column">
+            <p id="aboutmetext">
+              My name is Nikita Skobov. I like to make things. I work on a variety of open source projects, some of which you can find below. I studied statistics and computer science at the University of Minnesota. Sometimes I write about stuff on my blog: usually about software development, but not always.
+            </p>
+          </div>
+        </div>
+        <hr />
+      </div>
     </div>
   )
 }
