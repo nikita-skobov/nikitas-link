daaba0b6d10a10d9adc722dbf2cb7c7373bc5f0b
nikita-skobov <nskobov.websites@gmail.com>
2021-05-02 21:51:43 -0500

    adds card text/title and opacity transitions

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 function ProjectCard(props: any) {
-    const { card_data } = props
+  const { card_data } = props
+  const {
+    card_img,
+    card_text,
+    card_title,
+  } = card_data
   return (
-    <div className="col">
-        <img className="card" src={card_data} />
-    </div>
+    <a className="col card" href="#">
+        <img className="cardimg" src={card_img} />
+        <div className="cardtitle">{card_title}</div>
+        <div className="cardtext">{card_text}</div>
+    </a>
   )
 }
 
--- src/ProjectContainer.tsx
+++ src/ProjectContainer.tsx
@@ -3,10 +3,10 @@ import ProjectCard from './ProjectCard'
 import './ProjectContainer.css'
 
 const project_list = [
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F8a%2FTest_image_landscape_NO_TEXT.svg%2F1280px-Test_image_landscape_NO_TEXT.svg.png&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F8a%2FTest_image_landscape_NO_TEXT.svg%2F1280px-Test_image_landscape_NO_TEXT.svg.png&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F8a%2FTest_image_landscape_NO_TEXT.svg%2F1280px-Test_image_landscape_NO_TEXT.svg.png&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F8a%2FTest_image_landscape_NO_TEXT.svg%2F1280px-Test_image_landscape_NO_TEXT.svg.png&f=1&nofb=1",
+    { card_title: "a342 r32432432", card_text: "dsadsa 32 ewfe few", card_img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F8a%2FTest_image_landscape_NO_TEXT.svg%2F1280px-Test_image_landscape_NO_TEXT.svg.png&f=1&nofb=1" },
+    { card_title: "a342 r32432432", card_text: "dsadsa 32 ewfe few", card_img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftodaysmama.com%2F.image%2Fc_limit%252Ccs_srgb%252Cfl_progressive%252Cq_auto%3Agood%252Cw_700%2FMTU5OTA5MTcyNTEzNjc4ODgw%2Fimage-placeholder-title.jpg&f=1&nofb=1" },
+    { card_title: "a342 r32432432", card_text: "dsadsa 32 ewfe few", card_img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F8a%2FTest_image_landscape_NO_TEXT.svg%2F1280px-Test_image_landscape_NO_TEXT.svg.png&f=1&nofb=1" },
+    { card_title: "a342 r32432432", card_text: "dsadsa 32 ewfe few", card_img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F8a%2FTest_image_landscape_NO_TEXT.svg%2F1280px-Test_image_landscape_NO_TEXT.svg.png&f=1&nofb=1" },
 ]
 
 function ProjectRow(props: any) {
